467218c2976f3b1486b98a6305eee429e5a835a1
liushuyu <liushuyu011@gmail.com>
2021-08-04 02:17:27 -0600

    meta: add new repo scripts

---
+++ update_mirror.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+echo 'Scanning mainline packages ...'
+p-vector -c /etc/p-vector/aosc-os.toml full
+find /mirror/debs/dists -type d -exec chmod o+x {} \;
+
+echo 'Scanning retro packages ...'
+p-vector -c /etc/p-vector/aosc-os-retro.toml full
+find /mirror/debs-retro/dists -type d -exec chmod o+x {} \;
+
+date +%s > /mirror/last_update
+
+echo 'Running Topic generator ...'
+RUST_LOG=info /usr/local/bin/topic-manifest -d /mirror/debs/ -p 'AOSC-Dev/aosc-os-abbs'
